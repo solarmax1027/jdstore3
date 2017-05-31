0e1497833a0e8978bf43d74948d7bed9f4494637
solarmax <solarmax1027@gmail.com>
2017-06-01 07:08:08 +0800

    add lost paymethod

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -37,8 +37,7 @@ class OrdersController < ApplicationController
 
    def pay_with_alipay
     @order = Order.find_by_token(params[:id])
-    @order.set_payment_with!("alipay")
-
+    @order.set_payment_with("alipay")
     @order.make_payment!
 
     redirect_to order_path(@order.token), notice: "使用支付宝成功完成付款"
@@ -46,7 +45,7 @@ class OrdersController < ApplicationController
 
   def pay_with_wechat
     @order = Order.find_by_token(params[:id])
-    @order.set_payment_with!("wechat")
+    @order.set_payment_with("wechat")
 
    @order.make_payment!
 
