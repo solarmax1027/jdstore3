08805ccf01970416f8c9cbac07c4069a8d957d74
solarmax <solarmax1027@gmail.com>
2017-06-01 21:16:46 +0800

    add apply_cancel function

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -54,6 +54,13 @@ class OrdersController < ApplicationController
 
 
 
+  def apply_to_cancel
+    @order = Order.find_by_token(params[:id])
+    OrderMailer.apply_cancel(@order).deliver!
+    flash[:notice] = "已提交申请"
+    redirect_to :back
+  end
+
 
 
    private
